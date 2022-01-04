3afd1a04d07a85d171fd454a01b4c90e5653cb0f
pRob3 <bubblan@slaf.se>
2022-01-04 17:11:48 +0100

    Item Element component

---
+++ Organize.WASM/Components/ItemElement.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Organize.Shared.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organize.WASM.Components
+{
+    public partial class ItemElement<TItem> : ComponentBase where TItem : BaseItem
+    {
+        [Parameter]
+        public TItem Item { get; set; }
+    }
+}
