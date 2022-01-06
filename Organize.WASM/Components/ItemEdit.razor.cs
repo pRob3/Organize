d8f1207aadc913f05d6b3cfd14d318ca1c160cbf
pRob3 <bubblan@slaf.se>
2022-01-06 18:36:19 +0100

    Item edit, style and add bootstrap

---
+++ Organize.WASM/Components/ItemEdit.razor.cs
@@ -0,0 +1,16 @@
+﻿using Organize.Shared.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organize.WASM.Components
+{
+    public partial class ItemEdit
+    {
+        private BaseItem Item { get; set; } = new BaseItem();
+
+        private int TotalNumber { get; set; }
+    }
+}
