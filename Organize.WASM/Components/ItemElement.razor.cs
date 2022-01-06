41301b57261a2dfc574f38d8b2c364c26d4129dd
pRob3 <bubblan@slaf.se>
2022-01-06 06:46:13 +0100

    SASS styled item list

--- Organize.WASM/Components/ItemElement.razor.cs
+++ Organize.WASM/Components/ItemElement.razor.cs
@@ -20,6 +20,12 @@ namespace Organize.WASM.Components
         [Parameter]
         public TItem Item { get; set; }
 
+        [CascadingParameter]
+        public string ColorPrefix { get; set; }
+
+        [CascadingParameter]
+        public int TotalNumber { get; set; }
+
         private string DetailAreaId { get; set; }
 
         protected override void OnParametersSet()
